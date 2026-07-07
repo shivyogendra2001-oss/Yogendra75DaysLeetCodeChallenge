0ab909df7a9aa73e99034ffc132cce44804dfb25
shivyogendra2001-oss <shiv.yogendra2001@gmail.com>
2026-07-07 06:46:47 -0700

    Time: 0 ms (100%), Space: 12.5 MB (20.14%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,14 @@
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        result = []
+        def inorder(node):
+            if node is None:
+                return
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        return result 
+
